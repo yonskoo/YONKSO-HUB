3d1843c4be0f55d4d9814f5fcb759881bcf033f0
yonsko <mashhourm238@gmail.com>
2026-01-28 01:47:04 +0200

    Update loading-screen.tsx

--- src/components/loading-screen.tsx
+++ src/components/loading-screen.tsx
@@ -45,7 +45,7 @@ export function LoadingScreen() {
   <div className="w-full flex justify-center overflow-hidden">
   <div
     style={{
-      transform: "scale(0.8) translateX(-5%)",
+      transform: "scale(0.8) translateX(-7%)",
       transformOrigin: "center",
     }}
   >
