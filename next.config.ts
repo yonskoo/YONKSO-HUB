f0473f0807a0b0ff268a94b0009dc75048ae4a69
yonsko <mashhourm238@gmail.com>
2026-02-02 23:30:07 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
