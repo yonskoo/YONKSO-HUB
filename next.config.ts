0017f0f912070ad5ebee6762cc35a8fc27e69c33
yonsko <mashhourm238@gmail.com>
2026-02-02 22:55:02 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  distDir: "dist",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
